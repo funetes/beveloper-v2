5c8ea0a5183f7dadcb3ed83c6fcd2415ed559b36
kimhwan <kimhwan@kimhwanui-MacBookPro.local>
2021-02-01 22:31:39 +0900

    initial setting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,36 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from 'next/link';
+import useSWR from 'swr';
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+type post = {
+  body: string;
+  id: number;
+  title: string;
+  userId: number;
+};
 
-export default IndexPage
+const IndexPage = () => {
+  const { data, error } = useSWR(
+    'https://jsonplaceholder.typicode.com/posts',
+    fetcher
+  );
+
+  if (!data) return <>loading...</>;
+  const posts: post[] = data;
+
+  return (
+    <>
+      <h1>Hello Next.js 👋</h1>
+      <p>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </p>
+      {posts.map((post: post) => (
+        <h1>{post.body}</h1>
+      ))}
+    </>
+  );
+};
+
+export default IndexPage;
